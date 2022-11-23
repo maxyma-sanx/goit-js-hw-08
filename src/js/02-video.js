696f900c46659bfecf2c136845c16b3f141665b4
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-11-23 12:09:45 +0200

    task-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const PLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+let setTime;
+const currentTime = localStorage.getItem(PLAYER_CURRENT_TIME);
+
+function checkCurrentTime(time) {
+  time ? (setTime = currentTime) : (setTime = 0);
+}
+
+function onTimeUpdate(data) {
+  localStorage.setItem(PLAYER_CURRENT_TIME, data.seconds);
+}
+
+window.addEventListener('load', checkCurrentTime(currentTime));
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+player.setCurrentTime(setTime);
